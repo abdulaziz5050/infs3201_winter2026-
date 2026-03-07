6afb8018d818bdda237b7b56134972686775cf00
abdulaziz5050 <abdulaziz5050@users.noreply.github.com>
2026-03-07 03:35:57 +0200

    Setup Handlebars view engine and created views directory

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const { connectDB } = require("./db.js");
 const app = express();
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Initialize MongoDB connection before starting the server
 connectDB().then(() => {
-    console.log("Connected to MongoDB strongly");
+    console.log("Connected to MongoDB ");
     
     app.listen(3000, () => {
-        console.log("Server running on port 3000");
+        console.log("Server on port 3000");
     });
 }).catch(err => {
     console.error("Failed to connect to MongoDB", err);
 });
 
-// Basic route to check if server is working
 app.get("/", (req, res) => {
-    res.send("INFS3201 Winter 2026 Server is Running");
+    res.send("Server is Running");
 });
